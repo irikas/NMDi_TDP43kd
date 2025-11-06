3bdb13b0478a5b0c32b3e6e2a881c31f2b843585
irikas <sinhairika@gmail.com>
2025-11-06 13:11:52 -0800

    Update 251105_nfcore_clipseq.sh
    Loop across the files since diff UMIs

--- 251105_nfcore_clipseq.sh
+++ 251105_nfcore_clipseq.sh
@@ -12,24 +12,27 @@
 MAIN_DIR="/data/jling2/irika/clipdata/251028_Tollervey_TDPCLIP_E-MTAB-530"
 GENOME_DIR="/data/jling2/irika/bin/genomes/GRCh38_v115"
 OUT_DIR="${MAIN_DIR}/output/$(date +"%Y%m%d_%H%M%S")"
-DESIGN_FILE="${MAIN_DIR}/input/251105_eclip_design.csv"
 SING_DIR="$HOME/singularity"
 export SINGULARITY_TMPDIR=$SING_DIR/tmp
 export SINGULARITY_CACHEDIR=$SING_DIR/singularity_cache
-mkdir -p $SING_DIR $SINGULARITY_TMPDIR $SINGULARITY_CACHEDIR $OUT_DIR
-
+mkdir -p $SING_DIR $SINGULARITY_TMPDIR $SINGULARITY_CACHEDIR $OUT_DIR ${OUT_DIR}/ERR03985{0..5}
 # ---------------------------
 # Load modules
 # ---------------------------
-ml load singularity/3.8.7
+ml load Java/11.0.2 singularity/3.8.7
 
 # ---------------------------
 # Run nf-core/clipseq
+# GRCh37 bc this version of iCount is not compatible with GRCh38
 # ---------------------------
-NXF_VER=22.10.6 /data/jling2/irika/bin/nextflow run nf-core/clipseq --input "${DESIGN_FILE}" --outdir "${OUT_DIR}" \
-  --fasta "${GENOME_DIR}/Homo_sapiens.GRCh38.dna.primary_assembly.fa" \
-  --fai "${GENOME_DIR}/Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai" \
-  --gtf "${GENOME_DIR}/Homo_sapiens.GRCh38.115.gtf " \
-  --star_index "${GENOME_DIR}/indices" \
-  --peakcaller "iCount" --motif true \
-  --email isinha1@jh.edu -profile singularity
+
+for file in 251106_*; do \
+  fName=${file##251106_eclip_design_}
+  fName=${fName%%.csv}
+  NXF_VER=22.10.6 /data/jling2/irika/bin/nextflow run nf-core/clipseq --input "${MAIN_DIR}/input/${file}" --outdir "${OUT_DIR}/ERR0398${fName}" \
+    --genome "GRCh37" \
+    --peakcaller "iCount" --motif true --max_cpus 28 --max_time 72.h \
+    --email isinha1@jh.edu -profile singularity
+done
+
+
